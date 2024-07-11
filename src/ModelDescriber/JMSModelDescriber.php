6570f7ac9af8817b6f78ef8625d52b40df4854f2
Thomas <thomas@sctr.net>
2024-07-11 12:16:33 -0500

    remove bad code

--- src/ModelDescriber/JMSModelDescriber.php
+++ src/ModelDescriber/JMSModelDescriber.php
@@ -149,14 +149,14 @@ class JMSModelDescriber implements ModelDescriberInterface, ModelRegistryAwareIn
             if (null !== $item->getter) {
                 try {
                     $reflections[] = new \ReflectionMethod($item->class, $item->getter);
-                } catch (\ReflectionException $ignored) {
+                } catch (\ReflectionException) {
                 }
             }
-            $this->checkRequiredFields($reflections, $schema, $name);
+
             if (null !== $item->setter) {
                 try {
                     $reflections[] = new \ReflectionMethod($item->class, $item->setter);
-                } catch (\ReflectionException $ignored) {
+                } catch (\ReflectionException) {
                 }
             }
 
@@ -396,36 +396,6 @@ class JMSModelDescriber implements ModelDescriberInterface, ModelRegistryAwareIn
             $this->propertyTypeUseGroupsCache[$type['name']] = null;
 
             return null;
-        }
-    }
-
-    /**
-     * Mark property as required if it is not nullable.
-     *
-     * @param array<\ReflectionProperty|\ReflectionMethod> $reflections
-     */
-    private function checkRequiredFields(array $reflections, OA\Schema $schema, string $name): void
-    {
-        foreach ($reflections as $reflection) {
-            $nullable = false;
-            if ($reflection instanceof \ReflectionProperty) {
-                $type = PHP_VERSION_ID >= 70400 ? $reflection->getType() : null;
-                if (null !== $type && !$type->allowsNull()) {
-                    $nullable = true;
-                }
-            } elseif ($reflection instanceof \ReflectionMethod) {
-                $returnType = $reflection->getReturnType();
-                if (null !== $returnType && !$returnType->allowsNull()) {
-                    $nullable = true;
-                }
-            }
-            if ($nullable) {
-                $required = Generator::UNDEFINED !== $schema->required ? $schema->required : [];
-                $required[] = $name;
-
-                $schema->required = $required;
-                break;
-            }
         }
     }
 }
